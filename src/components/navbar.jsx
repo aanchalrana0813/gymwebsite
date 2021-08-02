3e3cc44507b6e37964f8f9d7420dbdc5d4bb3fdf
unknown <aanchalannu02@gmail.com>
2021-08-02 12:19:53 +0530

    new

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../Images/download.png'
+
+export const navbar = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(navbar)
